b669c74358bf6f266133ccbaee61c4cfc99583b9
Sergey <aseev-1977@list.ru>
2022-12-23 23:26:47 +0300

    Задача 13

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите числа: ");
+int Num2 = int.Parse(Console.ReadLine());
+
+int result = ReturnThreeNumber(Num2);
+if (result == 0)
+{
+    Console.Write("Третьей цифры нет");
+}
+else
+{
+    Console.Write($"Третья цифра числа {Num2} -> {result}");
+}
+int ReturnThreeNumber(int num)
+{
+    int result = 0;
+    while (num > 999) { num /= 10; }
+    if (num > 99) { result = num % 10; }
+    return result;
+}
